030c47ff3b4b361e6e76d77b27a59cda93c2e9cc
achupradeep <achupradeep3050@gmail.com>
2025-04-25 10:48:39 +0530

    Implement exit and complete button

--- gui.py
+++ gui.py
@@ -8,9 +8,14 @@ list_box = sg.Listbox(values = functions.get_todos() , key = 'todos',
                       enable_events=True, size=[45,10])
 
 edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
 window = sg.Window('MY To-Do App',
-                   layout=[[label],[input_box, add_button],[list_box ,edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box ,edit_button , complete_button],
+                           [exit_button]],
                    font=('Helvetica',20))
 
 while True:
@@ -26,7 +31,7 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
-        case 'edit':
+        case 'Edit':
             todo_to_edit = values['todos'][0]
             new_todo = values['todo']
 
@@ -36,6 +41,16 @@ while True:
             functions.write_todos(todos)
             window['todos'].update(values=todos)
 
+        case 'Complete':
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update('')
+
+        case'Exit':
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
 
